bfc70bb97d0b31a054023c780c850da6740238a4
Ksmith106 <77019810+Ksmith106@users.noreply.github.com>
2022-03-29 08:57:51 -0600

    coffeeshopstorefront

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+ import {PageTitle} from './../components/PageTitle'
+
+export default function Home() {
+ 
+
+  return (
+   
+     <>
+       <PageTitle title="StoreFront" tagline="featured products"/>
+     
+     </>
+  )
+}
+
+function ListItem({number, key,  props}){
+  return <li>{number} <span>{key}</span></li>
+}
