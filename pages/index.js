601345ffa9042ac8351fd51dcf3382eca5cb937a
Ksmith106 <77019810+Ksmith106@users.noreply.github.com>
2022-04-08 07:54:20 -0600

    storefront

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,47 @@
- import {PageTitle} from './../components/PageTitle'
+import {useState, useEffect} from 'react'
+import PageTitle from "../components/PageTitle/PageTitle";
+import {Button} from "../components/Button"
+import {User} from '../components/User'
+/* 
+      CRA version of data loading
+      assessment 4
+      loading data=====>api   Firebase SDK   ref,  get(ref)
+*/
 
 export default function Home() {
- 
-
-  return (
-   
-     <>
-       <PageTitle title="StoreFront" tagline="featured products"/>
+    const [isLoading, setIsLoading] = useState(false);
+    const [userData, setUserData] = useState([])
      
+    useEffect(()=>{
+     
+        async function loadExternalDataTheCRAWay(){
+          const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
+          const data = await res.json()
+          setUserData(data)
+        }
+        loadExternalDataTheCRAWay()
+    },[])
+           
+ 
+  return (
+     <>
+     <PageTitle title="StoreFront" tagline="Featured Coffee"/> 
+     <div style={{textAlign:"center"}}>
+     <Button 
+     style={{margin:"2rem 0 0",}}
+     onClick={()=>setIsLoading(true)}
+     >Order Some Coffee</Button>
+     {
+       isLoading && <p style={{padding:"1rem"}}>This Is My Output</p>
+     }
+     </div>
+     <main>
+        {
+          userData.map(({id, name, email, username}) => <User key={id} name={name} email={email} username={username}/>)
+        }
+     </main>
      </>
-  )
+     )
 }
 
-function ListItem({number, key,  props}){
-  return <li>{number} <span>{key}</span></li>
-}
+
